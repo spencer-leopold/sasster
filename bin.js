0e8763b3d7363d299d0f3631839493153f34a03c
Spencer Leopold <spencer.leopold@gmail.com>
2015-07-05 00:59:26 -0400

    migrating repo

---
+++ bin.js
@@ -0,0 +1,18 @@
+#!/usr/bin/env node
+
+var Sasster = require('./lib/sasster');
+var argv = require('yargs')
+    .usage('Usage: sasster <source> <dest> --cwd [str] --imagePath [str] --outputStyle [str] --sourceMap [bool] -c [str] -w [bool]')
+    .describe('cwd', 'Current Working Directory, [required]')
+    .describe('imagePath', 'Path to image directory')
+    .describe('outputStyle', 'Style of output CSS (compress, nested, expanded)')
+    .describe('sourceMap', 'Whether to generate source maps')
+    .alias('c', 'config')
+    .describe('c', 'Path to custom config')
+    .alias('w', 'watch')
+    .describe('w', 'Whether to watch source directory')
+    .help('h')
+    .alias('h', 'help')
+    .argv;
+
+var e = Sasster(null, argv);
---
+++ index.js
@@ -0,0 +1,54 @@
+var path = require('path');
+var fs = require('fs');
+var Sasster = require('./lib/sasster');
+
+module.exports = function init(o, args) {
+  var options;
+
+  if (!!o) {
+    if (typeof o !== 'object' || Array.isArray(o)) {
+      throw new Error('Sasster options must be an object');
+    }
+
+    options = o;
+  }
+  else if (args && (!!args.config || (!!args._ && args._.length))) {
+    if (args._.length) {
+      options = args;
+      options.src = args._[0];
+      options.dest = args._[1];
+    }
+    else {
+      try {
+        options = require(args.config);
+      }
+      catch (e) {
+        throw e;
+      }
+    }
+  }
+  else {
+    try {
+      var package = require(path.resolve('package.json'));
+
+      if (!!package.sasster) {
+        options = package.sasster;
+      }
+      else {
+        var config = path.resolve('sasster.config.js');
+
+        if (fs.existsSync(config)) {
+          options = require(config);
+        }
+        else {
+          throw new Error('Cannot find sasster.config.js configuration');
+        }
+      }
+    }
+    catch (e) {
+      throw e;
+    }
+  }
+
+  return new Sasster(options, args);
+}
